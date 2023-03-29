3747b9828d259e81ddbca5567f46146f938c3ef5
Sabrina Tolay <sabrinatolay@gmail.com>
2023-03-29 15:10:38 -0300

    archivo python añadido

---
+++ probando.py
@@ -0,0 +1,9 @@
+print('hola mundo')
+class Animales:
+    def __init__(self,especie,nombre):
+        self.especie=especie
+        self.nombre=nombre
+
+perro=Animales("perruno","toby")
+print(perro.especie)
+print(perro.nombre)
